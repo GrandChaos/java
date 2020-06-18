64714a8ae496413e33a5109f569f0096efbbb2d2
GrandChaos <46842959+GrandChaos@users.noreply.github.com>
2020-06-18 10:12:24 +0300

    Add files via upload

---
+++ 1.8/src/ru/sbrf/jc/demo/Main.java
@@ -0,0 +1,27 @@
+package ru.sbrf.jc.demo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Что это такое: синий, большой, с усами и полностью набит зайцами?");
+	    for (int i = 0; i < 3; i++){
+            Scanner in = new Scanner(System.in);
+	        String answer = in.nextLine();
+	        switch (answer){
+                case "Троллейбус":
+                    System.out.println("Правильно!");
+                    i = 3;
+                    break;
+                case "Сдаюсь":
+                    System.out.println("Правильный ответ: Троллейбус");
+                    i = 3;
+                    break;
+                default:
+                    System.out.println("Подумай ещё");
+                    break;
+            }
+        }
+    }
+}
