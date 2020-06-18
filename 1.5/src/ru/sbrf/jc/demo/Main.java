c90588f3081c086d144c1864ae99a0f8f4ed0dd3
GrandChaos <46842959+GrandChaos@users.noreply.github.com>
2020-06-18 10:06:17 +0300

    Add files via upload

---
+++ 1.5/src/ru/sbrf/jc/demo/Main.java
@@ -0,0 +1,20 @@
+package ru.sbrf.jc.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    for (int num = 2; num <= 100; num++) {
+            boolean simple = true;
+            if ((num % 2 != 0) || (num == 2)) {
+                for (int i = 3; i * 2 < num; i += 2)
+                    if (num % i == 0) {
+                        simple = false;
+                        break;
+                    }
+            }
+            else simple = false;
+            if (simple == true)
+                System.out.print(num + " ");
+        }
+    }
+}
