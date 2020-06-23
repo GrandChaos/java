8c7f42337ff80c91260f292bc07728f6270b7ffa
GrandChaos <46842959+GrandChaos@users.noreply.github.com>
2020-06-23 09:00:35 +0300

    Update Main.java

--- 1.6/src/ru/sbrf/jc/demo/Main.java
+++ 1.6/src/ru/sbrf/jc/demo/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
 	for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 5; j++)
-            System.out.print("* ");
-        System.out.println();
-	    }
+		for (int j = 0; j < 5; j++)
+		    System.out.print("* ");
+		System.out.println();
+	}
     }
 }
